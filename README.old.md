eb42e57251c3bdfa92da62b4c902290954b9b916
dastansam <dastan.samatov.33@gmail.com>
2021-09-30 20:49:22 +0300

    init react app

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# bcn-fan-page
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Matches from './pages/landing';
+
+function App() {
+  return (
+    <div className="App">
+      <Matches />
+    </div>
+  );
+}
+
+export default App;
